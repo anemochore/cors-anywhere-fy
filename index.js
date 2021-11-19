33946a416fd4350897c3a27225b31365a6147461
anemochore <fallensky@naver.com>
2021-11-19 15:46:36 +0900

    test4

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
-process.env.CORSANYWHERE_WHITELIST = 'https://www.dropbox.com';  //dev
+//process.env.CORSANYWHERE_WHITELIST = 'https://www.dropbox.com';  //dev
 
-console.log(process.env.CORSANYWHERE_WHITELIST)
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    
     originWhitelist: (process.env.CORSANYWHERE_WHITELIST || '').split(','),
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
