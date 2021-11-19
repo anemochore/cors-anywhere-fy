d5d504a046dc2d1a5307ec2c3eb7d32593018ff5
anemochore <fallensky@naver.com>
2021-11-19 14:28:03 +0900

    first commit3

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: (process.env.CORSANYWHERE_WHITELIST || '').split(','),
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
